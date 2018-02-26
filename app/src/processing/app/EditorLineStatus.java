d7d5e680bc02ebef06ffba3cffe55130d5a7a2ad
PaulStoffregen <paul@pjrc.com>
2018-02-26 06:51:25 -0800

    Trim excessive Linux syspath for status bar display

--- app/src/processing/app/EditorLineStatus.java
+++ app/src/processing/app/EditorLineStatus.java
@@ -127,6 +127,12 @@ public class EditorLineStatus extends JComponent {
   }
 
   public void setSerialPort(String serialport) {
+    if (serialport.length() > 26) {
+      int pos = serialport.lastIndexOf("usb");
+      if (pos > 0) {
+        serialport = serialport.substring(pos);
+      }
+    }
     this.serialport = serialport;
   }
 
